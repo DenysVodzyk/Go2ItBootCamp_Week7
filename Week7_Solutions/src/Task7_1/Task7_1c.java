c620b442472db12428de7229bdc118737f94ca57
DenysVodzyk <vodzyk@gmail.com>
2020-12-20 18:34:42 -0500

    Solution to task 7.1c

---
+++ Week7_Solutions/src/Task7_1/Task7_1c.java
@@ -0,0 +1,27 @@
+package Task7_1;
+
+import java.io.IOException;
+
+public class Task7_1c {
+    public static void main(String[] args) {
+        int i = 2;
+
+        try {
+            if (i % 2 == 0) {
+                throw new RuntimeException("This is the test exception");
+            } else {
+                //normal execution
+            }
+        } catch (RuntimeException e) {
+            //handleRTE
+            System.out.println("Woohoo!I've caught RuntimeException");
+        } finally {
+            System.out.println("This code will be executed always!");
+            try {
+                throw new IOException("This is the test IO exception");
+            } catch (IOException e) {
+                System.out.println("Woohoo!I've caught IOException as well");
+            }
+        }
+    }
+}
